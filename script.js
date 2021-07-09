ee32d2221353d6cb54e1890c917522a3aa3a2d12
TsykalanovDima <86851333+TsykalanovDima@users.noreply.github.com>
2021-07-09 14:37:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const arr = [1, 2, 3, 4, 5];
+
+for (i = 0; i < arr.length ; i++) {
+    let sum = arr[i]**2
+    alert(sum)
+    console.log(sum);
+}
+
+for (i=1;i<100;i++)alert(i)
+
+for (i=100;i>1;i--)alert(i)
+
+for (let i = 0; i < 100; i++) {
+
+    if ((i % 2)-1) {
+      alert( i );
+    }
